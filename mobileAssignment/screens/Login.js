9caf9978ce121ee1ce7a1376779532025a59ff18
BlackPhoenixNine <blackphoenixnine@gmail.com>
2020-03-02 15:16:11 +0000

    Created LoginScreen Layout

--- mobileAssignment/screens/Login.js
+++ mobileAssignment/screens/Login.js
@@ -1,22 +1,105 @@
 import React, { Component } from 'react'; 
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-paper';
 
 class Login extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        }
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Login</Text>
-            </View>
+            <KeyboardAvoidingView behavior='padding' style={styles.wrapper}>
+                
+                <View style={styles.container}>
+
+                    <View style={styles.header}>
+                        <Text style={styles.header}>Chittr</Text>
+                    </View>
+
+                    <View>
+                        <TextInput
+                            style={styles.textInput}
+                            placeholder='Username'
+                            onChangeText={(username) => this.setState({username})}
+                            underlineColorAndroid='transparent'/>
+
+                        <TextInput
+                            style={styles.textInput}
+                            placeholder='Password'
+                            onChangeText={(password) => this.setState({password})}
+                            underlineColorAndroid='transparent'/>
+
+                        <TouchableOpacity
+                            style={styles.btn}
+                            onPress={this.login}>
+                            <Text>Login</Text>
+                        </TouchableOpacity>
+
+                        <Text onPress={this.register} style={styles.register}>Register a new account.</Text>
+                    </View>
+
+                </View>
+                
+            </KeyboardAvoidingView>
         );
+    }
+
+    login = () => {
+        alert('login')
+    }
+
+    register = () => {
+        alert('register')
     }
 }
 
 const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        backgroundColor: '#5c6bc0'
+    },
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        paddingHorizontal: 30,
     },
+    header: {
+        fontFamily: 'Avenir Next',
+        color: '#ffffff',
+        marginTop: 48,
+        marginBottom: 60,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    textInput: {
+        marginBottom: 20,
+        alignSelf: "stretch",
+    },
+    text: {
+        fontFamily: 'Avenir Next',
+        color: '#ffffff'
+    },
+    btn: {
+        backgroundColor: '#26418f',
+        fontSize: 16,
+        borderRadius: 4,
+        paddingVertical: 12,
+        marginTop: 32,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    register: {
+        marginTop: 24,
+        fontWeight: '500',
+        fontSize: 14,
+        color: '#ffffff',
+        textAlign: "center",
+    }
 })
 
 export default Login;
