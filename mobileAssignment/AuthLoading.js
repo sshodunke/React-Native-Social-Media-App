5f612f7d5bf9706ede9464411cc92627ba12b307
BlackPhoenixNine <blackphoenixnine@gmail.com>
2020-03-14 20:54:56 +0000

    Implemented React Redux

--- mobileAssignment/AuthLoading.js
+++ mobileAssignment/AuthLoading.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import rootReducer from './reducers/index'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import App from './App'
+import { persistStore, persistReducer} from 'redux-persist'
+import AsyncStorage from '@react-native-community/async-storage';
+import {PersistGate} from 'redux-persist/es/integration/react';
 
-const store = createStore(rootReducer);
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(persistedReducer)
+
+const persistedStore = persistStore(store)
 
 class AuthLoading extends React.Component{
     render() {
         return (
             <Provider store={store}>
-                <App/>
+                <PersistGate persistor={persistedStore} loading={null}>
+                    <App/>
+                </PersistGate>
             </Provider>
         )
     }
